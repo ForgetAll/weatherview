11c483cd2893042a6ab713c1f7e993acbe5ea7ba
xiasuhuei321 <xiasuhuei321@163.com>
2017-09-04 16:20:51 +0800

    fix(MainActivity):fix test code's crash

--- app/src/main/java/com/xiasuhuei321/gank_kotlin/MainActivity.kt
+++ app/src/main/java/com/xiasuhuei321/gank_kotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.xiasuhuei321.gank_kotlin
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Handler
@@ -7,11 +8,12 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    var bmp = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher_round)
+    var bmp: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val iv_img = findViewById(R.id.iv_img) as ImageView
+        bmp = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher_round)
         iv_img.setImageBitmap(bmp)
         Handler().postDelayed({
             iv_img.setImageBitmap(JNIEntry.blur(bmp))
